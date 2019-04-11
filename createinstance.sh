8f625e7cdaa1cc230bf206f547fc20d12e2d4b18
sanbv001 <anubhav.srinivas@gmail.com>
2019-04-11 09:22:20 +0530

    creat

--- createinstance.sh
+++ createinstance.sh
@@ -1,1 +1,1 @@
-aws opsworks --region us-east-1 create-instance --stack-id 935450cc-61e0-4b03-a3e0-160ac817d2bb --layer-ids 5c8c272a-f2d5-42e3-8245-5bf3927cb65b --hostname myinstance1 --instance-type t2.micro --os "Amazon Linux"
+aws ec2 run-instances --image-id ami-007d5db58754fa284 --count 1 --instance-type t2.micro --key-name Bit --security-group-ids sg-0b908aed454a70086 --subnet-id subnet-a0b8d48e
