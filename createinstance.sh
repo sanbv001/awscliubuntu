2d5af0d71b84531243e8d698cb6bef25cc446fcb
sanbv001 <anubhav.srinivas@gmail.com>
2019-04-11 09:42:33 +0530

    term

--- createinstance.sh
+++ createinstance.sh
@@ -1,4 +1,4 @@
 
 aws ec2 stop instances --instance-ids i-07c953d64c2d6d127
 
-aws ec2 terminate-instance --instance-ids i-07c953d64c2d6d127
+aws ec2 terminate-instances --instance-ids i-07c953d64c2d6d127
